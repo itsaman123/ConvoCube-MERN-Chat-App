1822ead5fce978f95c02d85baf3f430c3b33530a
Aman Murya <m.amanmaurya49@gmail.com>
2024-02-05 22:44:30 +0530

    added bacekd url in cors

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://mernappbackend-gl1e.onrender.com",
     credentials: true,
   },
 });
