22f7b82de70444c88f5cf74706120383996d5a7c
Aman Murya <m.amanmaurya49@gmail.com>
2025-03-12 09:15:13 +0530

    deployment fixes

--- server/index.js
+++ server/index.js
@@ -36,7 +36,8 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "https://convocube-chat.vercel.app",
     credentials: true,
   },
 });
