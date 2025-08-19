35a366f646fa5fa0c01ac3d3357de6fae3635ee5
Aman Murya <m.amanmaurya49@gmail.com>
2025-08-19 20:38:29 +0530

    Enable socket.io server in all environments

--- server/index.js
+++ server/index.js
@@ -74,7 +74,8 @@ app.use('*', (req, res) => {
 });
 
 // Only set up server and socket.io in non-serverless environments
-if (process.env.NODE_ENV !== 'production' || !process.env.VERCEL) {
+if (true) {   // always run socket.io server
+
   const PORT = process.env.PORT || 5000;
   const server = app.listen(PORT, () =>
     console.log(`Server started on port ${PORT}`)
